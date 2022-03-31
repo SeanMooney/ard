2d09655fa3aa1f45cbe35d2b0999f579d6eb5698
Sean Mooney <work@seanmooney.info>
2022-03-31 18:55:04 +0100

    add openstack provioner to bootstrap script

--- bootstrap-repo.sh
+++ bootstrap-repo.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 [[ -e ".venv" ]] || python3 -m venv .venv
 . .venv/bin/activate
-pip install bindep
+pip install bindep wheel
 set -x
 which dpkg && bindep -b | xargs sudo apt install
 which rpm && bindep -b | xargs dnf apt install
 #pip install ansible=2.9
 pip install ansible\<5 ansible-core\<2.12.0 \
-libvirt-python molecule molecule-vagrant python-vagrant netaddr
-vagrant plugin install vagrant-libvirt
+    molecule molecule-vagrant python-vagrant netaddr molecule-openstack openstacksdk
+# this fails on nixos so install it seperatly until i figure out why.
+pip install libvirt-python
+which vagrant && vagrant plugin install vagrant-libvirt
+git submodule update --init --recursive
 set +x
