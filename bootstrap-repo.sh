6f6270b0523c0f5e6b1dc4708c0b12f7f46f7e7c
Artom Lifshitz <alifshit@redhat.com>
2022-04-25 14:13:29 -0400

    Fix bootstrap script and bindep for F35
    * Fix the dnf command line typo
    * Don't install weak deps with dnf (see inline comment)
    * Add needed dependencies to bindep.txt
    * Add check for user being in the 'libvirt' group
    * Switch machine type to q35 and remove topology, at least one of
      these seems necessary for the creates VMs to boot properly.

--- bootstrap-repo.sh
+++ bootstrap-repo.sh
@@ -4,7 +4,9 @@
 pip install bindep wheel
 set -x
 which dpkg && bindep -b | xargs sudo apt install
-which rpm && bindep -b | xargs dnf apt install
+# Don't install weak deps, as that will pull in vagrant-libvirt, which we want
+# to install manually later on.
+which rpm && bindep -b | xargs sudo dnf -y --setopt=install_weak_deps=False install
 #pip install ansible=2.9
 pip install ansible\<5 ansible-core\<2.12.0 \
     molecule molecule-vagrant python-vagrant netaddr molecule-openstack openstacksdk
@@ -12,4 +14,16 @@ pip install ansible\<5 ansible-core\<2.12.0 \
 pip install libvirt-python
 which vagrant && vagrant plugin install vagrant-libvirt
 git submodule update --init --recursive
+# At least on Fedora, the user running this needs to be in the libvirt group to
+# avoid authentication errors when running `molecule create`
+if [[ `cat /etc/redhat-release 2> /dev/null` =~ 'Fedora' ]]
+then
+    if [[ `groups` =~ 'libvirt' ]]
+    then
+        true
+    else
+        echo 'You need to be part of the libvirt group for this to work.'
+        exit
+    fi
+fi
 set +x
